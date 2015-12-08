cd053dc744115cee4ddc1bcbd08ccee55a9002aa
sergio] <rodrigo8902009@hotmail.com>
2015-12-07 18:32:53 -0600

    first commit

---
+++ cadanaextra.c
@@ -0,0 +1,40 @@
+#include<stdio.h>//libreria de E/S
+char cadena[50],bus,camb;
+char sub[50];
+int a=0;
+int b=0;
+int c; 
+int existe=0;
+int main() 
+{
+    scanf("%s",cadena); 
+ 	scanf("%s",&bus); 
+ 	scanf("%s",&camb); 
+ 	
+ 	for(c=0;cadena[c]!=' ';c++)
+{ 
+	if(cadena[c]==bus) 
+{ 
+	cadena[c]=camb;	
+} 
+} 
+	printf("%s\n",cadena);  
+{
+	b++;
+	a=0;
+}
+	if(sub[a]=='\0')
+{
+	existe=1;
+	printf("existe\n");
+}
+	if(existe==0)
+{
+	printf("no existe\n");
+	printf("%s\n",cadena);
+}
+	if(sub[a]=='\0')
+{
+	
+}
+}  
